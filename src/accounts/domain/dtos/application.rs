f6a28014bc3d854662b165e43f0a4940de885766
Samuel Galvão Elias <sgelias@outlook.com>
2022-10-19 21:54:17 -0300

    feat: implements the url generator into accounts
    main

--- src/accounts/domain/dtos/application.rs
+++ src/accounts/domain/dtos/application.rs
@@ -4,7 +4,7 @@ use uuid::Uuid;
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ApplicationDTO {
-    pub id: Uuid,
+    pub id: Option<Uuid>,
 
     pub name: String,
     pub description: String,
