35b296be881e36634c9df1b52e9d294f4ee2e776
Samuel Galvão Elias <sgelias@outlook.com>
2022-10-29 21:33:33 -0300

    feat: implements the user role deletion and updating for manager users
    main

---
+++ src/accounts/use_cases/managers/update_role_name_and_description.rs
@@ -0,0 +1,78 @@
+use crate::domain::{
+    dtos::{guest::UserRoleDTO, profile::ProfileDTO},
+    entities::{
+        manager::{
+            user_role_fetching::UserRoleFetching,
+            user_role_updating::UserRoleUpdating,
+        },
+        shared::default_responses::{FetchResponse, UpdateResponse},
+    },
+    utils::errors::MappedErrors,
+};
+
+use uuid::Uuid;
+
+/// This function allows only the update of name and description attributes of
+/// a single role.
+pub async fn update_role_name_and_description(
+    profile: ProfileDTO,
+    name: Option<String>,
+    description: Option<String>,
+    role_id: Uuid,
+    role_fetching_repo: Box<&dyn UserRoleFetching>,
+    role_updating_repo: Box<&dyn UserRoleUpdating>,
+) -> Result<UpdateResponse<UserRoleDTO>, MappedErrors> {
+    // ? ----------------------------------------------------------------------
+    // ? Check the profile permissions
+    //
+    // Check if the user has manager status. Return an error if not.
+    // ? ----------------------------------------------------------------------
+
+    if !profile.is_manager {
+        return Err(MappedErrors::new(
+            "Only manager user could perform such operation.".to_string(),
+            Some(true),
+            None,
+        ));
+    };
+
+    // ? ----------------------------------------------------------------------
+    // ? Fetch role from data persistence layer
+    // ? ----------------------------------------------------------------------
+
+    let mut user_role = match role_fetching_repo.get(role_id).await {
+        Err(err) => return Err(err),
+        Ok(res) => match res {
+            FetchResponse::NotFound(id, msg) => {
+                return Err(MappedErrors::new(
+                    format!(
+                        "Unable to update record ({}): {}",
+                        id,
+                        msg.unwrap()
+                    ),
+                    Some(true),
+                    None,
+                ));
+            }
+            FetchResponse::Found(role) => role,
+        },
+    };
+
+    // ? ----------------------------------------------------------------------
+    // ? Update value of fetched object
+    // ? ----------------------------------------------------------------------
+
+    if name.is_some() {
+        user_role.name = name.unwrap();
+    };
+
+    if description.is_some() {
+        user_role.description = description.unwrap();
+    };
+
+    // ? ----------------------------------------------------------------------
+    // ? Perform the updating operation
+    // ? ----------------------------------------------------------------------
+
+    role_updating_repo.update(user_role).await
+}
