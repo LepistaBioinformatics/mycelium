851aeeaa32af338226392c6df6d382be5e6b8ef1
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2022-12-24 12:14:39 -0300

    feat: include email-dto into the re-exports
    main

--- myc_api/public/lib.rs
+++ myc_api/public/lib.rs
@@ -3,6 +3,9 @@ pub mod extractor;
 /// This is a re-exportation from the myc core to allow users to import both
 /// from myc-api instead of the myc-core.
 pub use myc_core::{
-    domain::dtos::profile::{LicensedResourcesDTO, ProfileDTO},
+    domain::dtos::{
+        email::EmailDTO,
+        profile::{LicensedResourcesDTO, ProfileDTO},
+    },
     settings::DEFAULT_PROFILE_KEY,
 };
