6c41e707cd80fbcd559151642b2b7669ea6d7450
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2022-12-24 17:56:12 -0300

    refactor: finish the migration of the smtp adapters to the adapters feature
    main

--- ports/api/src/api/modules/manager.rs
+++ ports/api/src/api/modules/manager.rs
@@ -1,8 +1,9 @@
-use myc_core::adapters::smtp::message_sending::MessageSendingSqlDbRepository;
+//use myc_core::adapters::smtp::message_sending::MessageSendingSqlDbRepository;
 use myc_prisma::repositories::{
     manager::guest_user_registration::GuestUserRegistrationSqlDbRepository,
     shared::account_fetching::AccountFetchingSqlDbRepository,
 };
+use myc_smtp::repositories::message_sending::MessageSendingSqlDbRepository;
 use shaku::module;
 
 module! {
