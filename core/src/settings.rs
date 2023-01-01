054dbc0bf421f696754d51e1879c7367e8befe56
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2023-01-01 18:08:20 -0300

    feat: limit the visivility of the token-expiration-time config to crate
    main

--- core/src/settings.rs
+++ core/src/settings.rs
@@ -8,7 +8,7 @@ lazy_static! {
 
     /// A static value to be used on calculation of the tokens expiration time.
     #[derive(Debug)]
-    pub static ref TOKENS_EXPIRATION_TIME: i64 =
+    pub(crate) static ref TOKENS_EXPIRATION_TIME: i64 =
         match var_os("TOKENS_EXPIRATION_TIME") {
             Some(path) => {
                 path.into_string().unwrap().parse::<i64>().unwrap().into()
