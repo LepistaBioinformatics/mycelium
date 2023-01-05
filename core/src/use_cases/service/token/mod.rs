5774d59449b91b508f6ce4f3a0453658ea6eba1f
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2023-01-05 11:40:32 -0300

    feat: imlement endpoints for token management by service users
    main

--- core/src/use_cases/service/token/mod.rs
+++ core/src/use_cases/service/token/mod.rs
@@ -1,9 +1,9 @@
 mod clean_tokens_range;
-mod deregister_token;
 mod generate_token_expiration_time;
 mod register_token;
+mod validate_token;
 
 pub use clean_tokens_range::clean_tokens_range;
-pub use deregister_token::deregister_token;
 pub use generate_token_expiration_time::generate_token_expiration_time;
 pub(crate) use register_token::register_token;
+pub use validate_token::validate_token;
