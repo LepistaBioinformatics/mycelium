a01202fdb154dbae075f7770b55b5d9050f4e14d
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2023-01-05 13:55:12 -0300

    refactor: move the upgrade and downgrade actions from shared to staff use-cases group
    main

--- core/src/use_cases/staff/account/mod.rs
+++ core/src/use_cases/staff/account/mod.rs
@@ -1,3 +1,7 @@
 mod create_seed_staff_account;
+mod downgrade_account_status;
+mod upgrade_account_privileges;
 
 pub use create_seed_staff_account::create_seed_staff_account;
+pub use downgrade_account_status::downgrade_account_status;
+pub use upgrade_account_privileges::upgrade_account_privileges;
