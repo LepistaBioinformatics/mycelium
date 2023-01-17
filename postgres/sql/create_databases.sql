da7e840ad5b70956679eeae7df39ec88b7053be4
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2023-01-17 14:56:19 -0300

    test: include user and roles creation during postgresql docker-compose building phase
    main

--- postgres/sql/create_databases.sql
+++ postgres/sql/create_databases.sql
@@ -2,3 +2,21 @@
 
 SELECT 'CREATE DATABASE "mycelium"'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mycelium');\gexec
+
+-- Create user roles
+
+CREATE ROLE "myc-service-role";
+
+-- Assign permissions to the role
+
+GRANT CONNECT ON DATABASE mycelium TO "myc-service-role";
+GRANT USAGE ON SCHEMA public TO "myc-service-role";
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO "myc-service-role";
+
+-- Create user
+
+CREATE USER "myc-user" WITH PASSWORD 'myc-password';
+
+-- Include user in role
+
+GRANT "myc-service-role" TO "myc-user";
