ee01196533b9669559dc275ddf570b5d294d0d37
Samuel Galvão Elias <sgelias@outlook.com>
2023-04-14 09:26:41 -0300

    build: upgrade error-code migration model to include the is-native flag
    main

--- adapters/prisma/src/migrations/20230411142001_create_error_codes_model/migration.sql
+++ adapters/prisma/src/migrations/20230411142001_create_error_codes_model/migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE "error_code" (
     "message" VARCHAR(255) NOT NULL,
     "details" VARCHAR(255),
     "is_internal" BOOLEAN NOT NULL DEFAULT false,
+    "is_native" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "error_code_pkey" PRIMARY KEY ("prefix","code")
 );
