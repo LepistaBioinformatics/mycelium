f4de5ca7756d600005a9911e0f7f9cde1b0c29e1
Samuel Galvão Elias <sgelias@outlook.com>
2023-08-06 09:21:14 -0300

    feat: wip - implements entities for webhook management
    main

---
+++ core/src/domain/entities/webhook_registration.rs
@@ -0,0 +1,13 @@
+use crate::domain::dtos::webhook::WebHook;
+
+use async_trait::async_trait;
+use clean_base::{entities::CreateResponseKind, utils::errors::MappedErrors};
+use shaku::Interface;
+
+#[async_trait]
+pub trait WebHookRegistration: Interface + Send + Sync {
+    async fn create(
+        &self,
+        webhook: WebHook,
+    ) -> Result<CreateResponseKind<WebHook>, MappedErrors>;
+}
