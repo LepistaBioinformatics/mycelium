c149d9a9c7ddb1f716c9496e35799ee2b4ccf9bd
Samuel Galvão Elias <sgelias@outlook.com>
2023-12-14 16:47:52 -0300

    feat: implements webhook updating and listing
    main

---
+++ core/src/use_cases/roles/standard/system_manager/webhook/update_webhook.rs
@@ -0,0 +1,41 @@
+use crate::domain::{
+    actors::DefaultActor,
+    dtos::{profile::Profile, webhook::WebHook},
+    entities::WebHookUpdating,
+};
+
+use clean_base::{
+    entities::UpdatingResponseKind,
+    utils::errors::{factories::use_case_err, MappedErrors},
+};
+use uuid::Uuid;
+
+pub async fn update_webhook(
+    profile: Profile,
+    webhook: WebHook,
+    webhook_id: Uuid,
+    webhook_updating_repo: Box<&dyn WebHookUpdating>,
+) -> Result<UpdatingResponseKind<WebHook>, MappedErrors> {
+    let target_webhook_id = match webhook.id.to_owned() {
+        Some(id) => id,
+        None => {
+            return use_case_err(
+                "WebHook id is required to update a WebHook.".to_string(),
+            )
+            .as_error()
+        }
+    };
+
+    if webhook_id != target_webhook_id {
+        return use_case_err(
+            "WebHook id does not match the path id.".to_string(),
+        )
+        .as_error();
+    };
+
+    profile.get_update_ids_or_error(vec![
+        DefaultActor::SystemManager.to_string()
+    ])?;
+
+    webhook_updating_repo.update(webhook).await
+}
