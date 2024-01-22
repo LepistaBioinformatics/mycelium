28c169f42054d69884ee8d33953041260ca1d808
Samuel Galvão Elias <sgelias@outlook.com>
2024-01-21 22:54:57 -0300

    feat(subscription-account): implements a use-case and endpoint to update accounts name and flags
    main

--- core/src/use_cases/roles/standard/subscription_account_manager/account/mod.rs
+++ core/src/use_cases/roles/standard/subscription_account_manager/account/mod.rs
@@ -3,8 +3,10 @@ mod get_account_details;
 mod list_accounts_by_type;
 mod propagate_existing_subscription_account;
 mod propagate_subscription_account;
+mod update_account_name_and_flags;
 
 pub use create_subscription_account::*;
 pub use get_account_details::*;
 pub use list_accounts_by_type::*;
 pub use propagate_existing_subscription_account::*;
+pub use update_account_name_and_flags::*;
---
+++ core/src/use_cases/roles/standard/subscription_account_manager/account/update_account_name_and_flags.rs
@@ -0,0 +1,72 @@
+use crate::domain::{
+    actors::DefaultActor,
+    dtos::{account::Account, profile::Profile},
+    entities::{AccountFetching, AccountUpdating},
+};
+
+use clean_base::{
+    entities::{FetchResponseKind, UpdatingResponseKind},
+    utils::errors::{factories::use_case_err, MappedErrors},
+};
+use uuid::Uuid;
+
+pub async fn update_account_name_and_flags(
+    profile: Profile,
+    account_id: Uuid,
+    name: Option<String>,
+    is_active: Option<bool>,
+    is_checked: Option<bool>,
+    is_archived: Option<bool>,
+    is_default: Option<bool>,
+    account_fetching_repo: Box<&dyn AccountFetching>,
+    account_updating_repo: Box<&dyn AccountUpdating>,
+) -> Result<UpdatingResponseKind<Account>, MappedErrors> {
+    // ? -----------------------------------------------------------------------
+    // ? Check if the current account has sufficient privileges
+    // ? -----------------------------------------------------------------------
+
+    profile.get_default_update_ids_or_error(vec![
+        DefaultActor::SubscriptionAccountManager.to_string(),
+    ])?;
+
+    // ? -----------------------------------------------------------------------
+    // ? Fetch account
+    // ? -----------------------------------------------------------------------
+
+    let mut account = match account_fetching_repo.get(account_id).await? {
+        FetchResponseKind::Found(account) => account,
+        FetchResponseKind::NotFound(id) => {
+            return use_case_err(format!(
+                "Account with id {} not found",
+                id.unwrap_or_default()
+            ))
+            .as_error();
+        }
+    };
+
+    if let Some(name) = name {
+        account.name = name;
+    }
+
+    if let Some(is_active) = is_active {
+        account.is_active = is_active;
+    }
+
+    if let Some(is_checked) = is_checked {
+        account.is_checked = is_checked;
+    }
+
+    if let Some(is_archived) = is_archived {
+        account.is_archived = is_archived;
+    }
+
+    if let Some(is_default) = is_default {
+        account.is_default = is_default;
+    }
+
+    // ? -----------------------------------------------------------------------
+    // ? Return a positive response
+    // ? -----------------------------------------------------------------------
+
+    account_updating_repo.update(account).await
+}
