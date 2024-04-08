b517464eb598969fa0188590661cb353b09ff390
Samuel Galvão Elias <sgelias@outlook.com>
2024-04-07 22:08:19 -0300

    ci: implements action to publish a new version when a tag was created
    main

--- scripts/publish-all.sh
+++ scripts/publish-all.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/bash
 
+ARGS=("$@")
+
 # Base packages
-cargo publish -p mycelium-base
-cargo publish -p mycelium-config
-cargo publish -p myc-core
+cargo publish -p mycelium-base $ARGS
+cargo publish -p mycelium-config $ARGS
+cargo publish -p myc-core $ARGS
 
 # Adapters
-cargo publish -p mycelium-memory-db
-cargo publish -p mycelium-service
-cargo publish -p mycelium-smtp
+cargo publish -p mycelium-memory-db $ARGS
+cargo publish -p mycelium-service $ARGS
+cargo publish -p mycelium-smtp $ARGS
 
 # Ports and related
-cargo publish -p mycelium-http-tools
+cargo publish -p mycelium-http-tools $ARGS
