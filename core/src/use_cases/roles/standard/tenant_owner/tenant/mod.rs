1641760de97ecbb367594fb921cc897d40d1da64
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2024-10-02 14:45:12 -0300

    docs: include note about the roles that should perform actions on tenant-owner -- tenant use-cases
    main

--- core/src/use_cases/roles/standard/tenant_owner/tenant/mod.rs
+++ core/src/use_cases/roles/standard/tenant_owner/tenant/mod.rs
@@ -1,3 +1,10 @@
+// All actions listed below should ve performed by:
+//
+// - Tenant Owner
+// - Platform Managers
+// - Platform Staffs
+//
+
 mod update_tenant_archiving_status;
 mod update_tenant_name_and_description;
 mod update_tenant_trashing_status;
