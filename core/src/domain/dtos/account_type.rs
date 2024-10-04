b5de24379728ec79dc361575610fe57aca68c87e
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2024-10-04 08:43:08 -0300

    test: include a basic test for account-type
    main

--- core/src/domain/dtos/account_type.rs
+++ core/src/domain/dtos/account_type.rs
@@ -7,8 +7,17 @@ use uuid::Uuid;
 #[derive(Clone, Debug, Eq, PartialEq, Deserialize, Serialize, ToSchema)]
 #[serde(rename_all = "camelCase")]
 pub enum AccountTypeV2 {
-    Manager,
+    /// Staff account type
+    ///
+    /// Staff account type is a special account type that is used to represent
+    /// staff members in the system.
     Staff,
+
+    /// Manager account type
+    ///
+    /// Manager account type is a special account type that is used to represent
+    /// managers in the system.
+    Manager,
 
     /// User account type
     ///
@@ -19,23 +28,22 @@ pub enum AccountTypeV2 {
     ///
     /// A subscription account is a special account type that is used to
     /// represent legal entities that have a subscription to the service.
-    Subscription {
-        tenant_id: Uuid,
-    },
+    #[serde(rename_all = "camelCase")]
+    Subscription { tenant_id: Uuid },
 
     /// Role associated account type
     ///
     /// Role associated account type is an special type of account, created to
     /// connect users to a specific standard role in the application.
+    #[serde(rename_all = "camelCase")]
     StandardRoleAssociated {
         tenant_id: Uuid,
         role_name: ActorName,
         role_id: Uuid,
     },
 
-    TenantManager {
-        tenant_id: Uuid,
-    },
+    #[serde(rename_all = "camelCase")]
+    TenantManager { tenant_id: Uuid },
 }
 
 #[cfg(test)]
@@ -44,6 +52,9 @@ mod tests {
 
     #[test]
     fn test_account_type_v2_json_serialization() {
+        //
+        // Type AccountTypeV2::Subscription
+        //
         let account_type = AccountTypeV2::Subscription {
             tenant_id: Uuid::from_u128(0),
         };
@@ -54,5 +65,99 @@ mod tests {
             json,
             r#"{"subscription":{"tenantId":"00000000-0000-0000-0000-000000000000"}}"#
         );
+
+        let account_type: AccountTypeV2 = serde_json::from_str(&json).unwrap();
+
+        assert_eq!(
+            account_type,
+            AccountTypeV2::Subscription {
+                tenant_id: Uuid::from_u128(0)
+            }
+        );
+
+        //
+        // Type AccountTypeV2::StandardRoleAssociated
+        //
+        let account_type = AccountTypeV2::StandardRoleAssociated {
+            tenant_id: Uuid::from_u128(0),
+            role_name: ActorName::CustomRole("test".to_string()),
+            role_id: Uuid::from_u128(0),
+        };
+
+        let json = serde_json::to_string(&account_type).unwrap();
+
+        assert_eq!(
+            json,
+            r#"{"standardRoleAssociated":{"tenantId":"00000000-0000-0000-0000-000000000000","roleName":{"customRole":"test"},"roleId":"00000000-0000-0000-0000-000000000000"}}"#
+        );
+
+        let account_type: AccountTypeV2 = serde_json::from_str(&json).unwrap();
+
+        assert_eq!(
+            account_type,
+            AccountTypeV2::StandardRoleAssociated {
+                tenant_id: Uuid::from_u128(0),
+                role_name: ActorName::CustomRole("test".to_string()),
+                role_id: Uuid::from_u128(0),
+            }
+        );
+
+        //
+        // Type AccountTypeV2::TenantManager
+        //
+        let account_type = AccountTypeV2::TenantManager {
+            tenant_id: Uuid::from_u128(0),
+        };
+
+        let json = serde_json::to_string(&account_type).unwrap();
+
+        assert_eq!(
+            json,
+            r#"{"tenantManager":{"tenantId":"00000000-0000-0000-0000-000000000000"}}"#
+        );
+
+        let account_type: AccountTypeV2 = serde_json::from_str(&json).unwrap();
+
+        assert_eq!(
+            account_type,
+            AccountTypeV2::TenantManager {
+                tenant_id: Uuid::from_u128(0)
+            }
+        );
+
+        //
+        // Type AccountTypeV2::Manager
+        //
+        let account_type = AccountTypeV2::Manager;
+
+        let json = serde_json::to_string(&account_type).unwrap();
+
+        assert_eq!(json, r#""manager""#);
+
+        let account_type: AccountTypeV2 = serde_json::from_str(&json).unwrap();
+
+        assert_eq!(account_type, AccountTypeV2::Manager);
+
+        //
+        // Type AccountTypeV2::Staff
+        //
+        let account_type = AccountTypeV2::Staff;
+
+        let json = serde_json::to_string(&account_type).unwrap();
+
+        assert_eq!(json, r#""staff""#);
+
+        let account_type: AccountTypeV2 = serde_json::from_str(&json).unwrap();
+
+        assert_eq!(account_type, AccountTypeV2::Staff);
+
+        //
+        // Type AccountTypeV2::User
+        //
+        let account_type = AccountTypeV2::User;
+
+        let json = serde_json::to_string(&account_type).unwrap();
+
+        assert_eq!(json, r#""user""#);
     }
 }
