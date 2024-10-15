c97a71b2cbd763139aa064ba3f9567d729b544d8
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2024-10-15 15:26:37 -0300

    feat: implements the tenant-manager endpoint related elements
    feature/gh10

--- core/src/domain/entities/account/account_deletion.rs
+++ core/src/domain/entities/account/account_deletion.rs
@@ -1,11 +1,11 @@
+use crate::domain::dtos::related_accounts::RelatedAccounts;
+
 use async_trait::async_trait;
 use mycelium_base::{
     entities::DeletionResponseKind, utils::errors::MappedErrors,
 };
 use shaku::Interface;
 use uuid::Uuid;
-
-use crate::domain::dtos::related_accounts::RelatedAccounts;
 
 #[async_trait]
 pub trait AccountDeletion: Interface + Send + Sync {
