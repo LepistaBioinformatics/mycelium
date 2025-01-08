b701e2dafb11f0b74c1840909419567d682c55fb
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2025-01-08 13:38:53 -0300

    perf(tenant): remove from serialization none fields of tenant dto
    develop

--- core/src/domain/dtos/tenant/mod.rs
+++ core/src/domain/dtos/tenant/mod.rs
@@ -21,8 +21,14 @@ pub type TenantMeta = HashMap<TenantMetaKey, String>;
 #[derive(Clone, Debug, Deserialize, Serialize, ToSchema, Eq, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct Tenant {
+    /// The unique identifier of the tenant
     pub id: Option<Uuid>,
+
+    /// The name of the tenant
     pub name: String,
+
+    /// The description of the tenant
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
 
     /// The owner of the tenant
@@ -34,12 +40,14 @@ pub struct Tenant {
     /// The tenant manager
     ///
     /// The account of the tenant manager.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub manager: Option<Parent<Account, Uuid>>,
 
     /// The tags of the tenant
     ///
     /// This is the list of tags of the tenant. The tags are used to categorize
     /// the tenant. The tags are used to categorize the tenant.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub tags: Option<Vec<Tag>>,
 
     /// Meta information
@@ -47,6 +55,7 @@ pub struct Tenant {
     /// This is the meta information of the tenant. The meta information is a
     /// key-value pair of string. The key is the name of the meta information,
     /// and the value is the value of the meta information.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub meta: Option<HashMap<TenantMetaKey, String>>,
 
     /// The status of the tenant
@@ -54,12 +63,14 @@ pub struct Tenant {
     /// This is the status of the tenant. The status is a key-value pair of
     /// string. The key is the name of the status (defined in `StatusKey`), and
     /// the value is the value of the status.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub status: Option<Vec<TenantStatus>>,
 
     /// The date and time the tenant was created
     pub created: DateTime<Local>,
 
     /// The date and time the tenant was last updated
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub updated: Option<DateTime<Local>>,
 }
 
