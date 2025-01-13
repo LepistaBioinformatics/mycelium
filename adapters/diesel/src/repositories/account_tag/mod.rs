45e1d324458fe1b968356f2f757b50dad9b8e6c3
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2025-01-13 17:21:58 -0300

    refactor: move the account-tag to a dedicated module
    feature/gh29

---
+++ adapters/diesel/src/repositories/account_tag/mod.rs
@@ -0,0 +1,6 @@
+pub mod account_tag_deletion;
+pub mod account_tag_registration;
+pub mod account_tag_updating;
+pub use account_tag_deletion::*;
+pub use account_tag_registration::*;
+pub use account_tag_updating::*;
