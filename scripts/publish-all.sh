3e1350e54080348841fb21a6855327ccb3b28bf1
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2025-01-26 21:43:24 -0300

    build: include diesel and cli and api ports to the publishable packages
    feature/gh29

--- scripts/publish-all.sh
+++ scripts/publish-all.sh
@@ -8,9 +8,12 @@ cargo publish -p mycelium-config $ARGS
 cargo publish -p myc-core $ARGS
 
 # Adapters
+cargo publish -p mycelium-diesel $ARGS
 cargo publish -p mycelium-memory-db $ARGS
 cargo publish -p mycelium-service $ARGS
 cargo publish -p mycelium-notifier $ARGS
 
 # Ports and related
 cargo publish -p mycelium-http-tools $ARGS
+cargo publish -p mycelium-cli $ARGS
+cargo publish -p mycelium-api $ARGS
