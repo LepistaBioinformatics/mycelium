6f0b2120baeca2cdd5bd6a66eb7b394e877814ff
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2025-06-29 10:28:59 -0300

    feat(subscription-manager-account): create a use-case to initialize the subscription manager account with role scoped account type
    Previously the creation of a subscription management account should restricted to the creaion of a simple account and elevate to system account. Now, the subscription account management account will be created directly

--- core/src/use_cases/role_scoped/tenant_manager/account/create_subscription_manager_account.rs
+++ core/src/use_cases/role_scoped/tenant_manager/account/create_subscription_manager_account.rs
@@ -1,11 +1,21 @@
 use crate::domain::{
-    dtos::{account::Account, profile::Profile},
-    entities::AccountRegistration,
+    actors::SystemActor,
+    dtos::{
+        account::Account,
+        guest_role::{GuestRole, Permission},
+        native_error_codes::NativeErrorCodes,
+        profile::Profile,
+        written_by::WrittenBy,
+    },
+    entities::{AccountRegistration, GuestRoleRegistration},
 };
 
+use futures::future;
 use mycelium_base::{
-    entities::GetOrCreateResponseKind, utils::errors::MappedErrors,
+    entities::{CreateResponseKind, GetOrCreateResponseKind},
+    utils::errors::{use_case_err, MappedErrors},
 };
+use tracing::Instrument;
 use uuid::Uuid;
 
 /// Create a subscription manager account
@@ -19,12 +29,125 @@ use uuid::Uuid;
         profile_id = %profile.acc_id,
         owners = ?profile.owners.iter().map(|o| o.redacted_email()).collect::<Vec<_>>(),
     ),
-    skip(profile, account_registration_repo)
+    skip(profile, account_registration_repo, guest_role_registration_repo)
 )]
 pub async fn create_subscription_manager_account(
     profile: Profile,
     tenant_id: Uuid,
+    guest_role_registration_repo: Box<&dyn GuestRoleRegistration>,
     account_registration_repo: Box<&dyn AccountRegistration>,
-) -> Result<GetOrCreateResponseKind<Account>, MappedErrors> {
-    unimplemented!()
+) -> Result<CreateResponseKind<Account>, MappedErrors> {
+    // ? -----------------------------------------------------------------------
+    // ? Initialize tracing span
+    // ? -----------------------------------------------------------------------
+
+    let span = tracing::Span::current();
+
+    tracing::trace!("Starting to create a subscription manager account");
+
+    // ? -----------------------------------------------------------------------
+    // ? Check if the current account has sufficient privileges
+    // ? -----------------------------------------------------------------------
+
+    profile
+        .get_tenant_wide_permission_or_error(tenant_id, Permission::Write)?;
+
+    // ? -----------------------------------------------------------------------
+    // ? Get or create the role
+    //
+    // The role should be fetched from the database.
+    // ? -----------------------------------------------------------------------
+
+    let (id, slug, description, children, is_system) = (
+        None,
+        SystemActor::TenantManager.to_string(),
+        Some(format!(
+            "Subscription manager account for tenant: {}",
+            tenant_id
+        )),
+        None,
+        true,
+    );
+
+    // ? -----------------------------------------------------------------------
+    // ? Get or create the read/write roles
+    //
+    // The roles are fetched from the database if exists, otherwise they are
+    // created.
+    // ? -----------------------------------------------------------------------
+
+    let (read_role_result, write_role_result) = future::join(
+        guest_role_registration_repo
+            .get_or_create(GuestRole::new(
+                id,
+                slug.to_owned(),
+                description.to_owned(),
+                Permission::Read,
+                children.to_owned(),
+                is_system.to_owned(),
+            ))
+            .instrument(span.clone()),
+        guest_role_registration_repo
+            .get_or_create(GuestRole::new(
+                id,
+                slug.to_owned(),
+                description,
+                Permission::Write,
+                children,
+                is_system,
+            ))
+            .instrument(span.clone()),
+    )
+    .await;
+
+    let read_role_id = match read_role_result? {
+        GetOrCreateResponseKind::Created(role) => role,
+        GetOrCreateResponseKind::NotCreated(role, _) => role,
+    }
+    .id
+    .map_or_else(
+        || {
+            use_case_err(format!("Role ID is not set: {}", tenant_id))
+                .with_code(NativeErrorCodes::MYC00003)
+                .as_error()
+        },
+        |id| Ok(id),
+    )?;
+
+    let write_role_id = match write_role_result? {
+        GetOrCreateResponseKind::Created(role) => role,
+        GetOrCreateResponseKind::NotCreated(role, _) => role,
+    }
+    .id
+    .map_or_else(
+        || {
+            use_case_err(format!("Role ID is not set: {}", tenant_id))
+                .with_code(NativeErrorCodes::MYC00003)
+                .as_error()
+        },
+        |id| Ok(id),
+    )?;
+
+    // ? -----------------------------------------------------------------------
+    // ? Register the account
+    //
+    // The account are registered using the already created user.
+    // ? -----------------------------------------------------------------------
+
+    let mut unchecked_account = Account::new_role_related_account(
+        format!("tid/{}/{}", tenant_id, slug),
+        tenant_id,
+        read_role_id,
+        write_role_id,
+        slug,
+        true,
+        Some(WrittenBy::new_from_account(profile.acc_id)),
+    );
+
+    unchecked_account.is_checked = true;
+
+    account_registration_repo
+        .create_subscription_account(unchecked_account, tenant_id)
+        .instrument(span)
+        .await
 }
