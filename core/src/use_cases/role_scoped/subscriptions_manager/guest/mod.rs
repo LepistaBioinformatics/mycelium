685dc21bb60b28f688eb20f2d64eec549a0d7bb7
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2026-01-07 08:00:20 -0300

    feat: wip - create initial files to declare flags registration before implements

--- core/src/use_cases/role_scoped/subscriptions_manager/guest/mod.rs
+++ core/src/use_cases/role_scoped/subscriptions_manager/guest/mod.rs
@@ -8,9 +8,17 @@
 mod guest_user_to_subscription_account;
 mod list_guest_on_subscription_account;
 mod list_licensed_accounts_of_email;
+mod register_deny_flag;
+mod register_permit_flag;
+mod revoke_deny_flat;
+mod revoke_permit_flat;
 mod revoke_user_guest_to_subscription_account;
 
 pub use guest_user_to_subscription_account::*;
 pub use list_guest_on_subscription_account::*;
 pub use list_licensed_accounts_of_email::*;
+pub use register_deny_flag::*;
+pub use register_permit_flag::*;
+pub use revoke_deny_flat::*;
+pub use revoke_permit_flat::*;
 pub use revoke_user_guest_to_subscription_account::*;
